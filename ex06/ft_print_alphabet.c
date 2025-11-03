26c04bc134c51ca606db9c750e4983275a9c17d4
Hamza <hamzahamidi548@gmail.com>
2025-11-03 18:39:12 +0100

    first commit and add ex06 07 08 09

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamidi <hhamidi@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 14:22:46 by hhamidi           #+#    #+#             */
+/*   Updated: 2025/11/03 14:48:05 by hhamidi          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		write(1, &alphabet, 1);
+		alphabet++;
+	}
+}
